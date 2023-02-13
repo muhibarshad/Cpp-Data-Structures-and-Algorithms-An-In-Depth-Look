57146f5484a1e6e1d11a9be147fd7d18507c3326
haiderali780 <muhammadhaiderali782002@gmail.com>
2023-02-13 00:35:44 -0800

    added question for stack

--- 01_Data Structures/Linear-Data-Structures/04_Stack/Code/03_isValid_Expression_Using_stack.cpp
+++ 01_Data Structures/Linear-Data-Structures/04_Stack/Code/03_isValid_Expression_Using_stack.cpp
@@ -4,30 +4,35 @@ Determine if a given expression containing parentheses, curly braces, and square
 Time:07 Feb,2022 _11:45
 */
 
-
 #include <iostream>
 #include <stack>
 #include <string>
 
-bool isValidExpression(const std::string& expression) {
+bool isValidExpression(const std::string &expression)
+{
     std::stack<char> s;
-    for (char c : expression) {
-        switch (c) {
+    for (char c : expression)
+    {
+        switch (c)
+        {
         case '(':
         case '{':
         case '[':
             s.push(c);
             break;
         case ')':
-            if (s.empty() || (s.top() != '(')) return false;
+            if (s.empty() || (s.top() != '('))
+                return false;
             s.pop();
             break;
         case '}':
-            if (s.empty() || (s.top() != '{')) return false;
+            if (s.empty() || (s.top() != '{'))
+                return false;
             s.pop();
             break;
         case ']':
-            if (s.empty() || (s.top() != '[')) return false;
+            if (s.empty() || (s.top() != '['))
+                return false;
             s.pop();
             break;
         default:
@@ -37,11 +42,9 @@ bool isValidExpression(const std::string& expression) {
     return s.empty();
 }
 
-
-int main() {
+int main()
+{
     std::string expression = "{(})";
     std::cout << (isValidExpression(expression) ? "Expression is well-formed." : "Expression is not well-formed.") << std::endl;
     return 0;
 }
-
-
--- 01_Data Structures/Linear-Data-Structures/04_Stack/Code/04_decimal_to_binary.cpp
+++ 01_Data Structures/Linear-Data-Structures/04_Stack/Code/04_decimal_to_binary.cpp
@@ -4,32 +4,33 @@ Convert a decimal number to binary using a stack and return the result as an int
 Time:07 Feb,2022 _12:01
 */
 
-
 #include <iostream>
 #include <stack>
 #include <string>
 
 using namespace std;
 
-int decimalToBinary(int decimal) {
+int decimalToBinary(int decimal)
+{
     stack<int> binary;
-    while (decimal > 0) {
+    while (decimal > 0)
+    {
         binary.push(decimal % 2);
         decimal /= 2;
     }
     string result;
-    while (!binary.empty()) {
+    while (!binary.empty())
+    {
         result += to_string(binary.top());
         binary.pop();
     }
     return stoi(result);
 }
 
-int main() {
+int main()
+{
     int decimal = 10;
     int binary = decimalToBinary(decimal);
     cout << binary << endl;
     return 0;
 }
-
-
